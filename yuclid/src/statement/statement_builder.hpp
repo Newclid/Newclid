fa6e276c3a170fab5c39714c8a93c99d3878b06c
Simeon Vutov <s.vutov@student.tudelft.nl>
2026-05-06 16:40:56 +0200

    Added statement builder header file with a function to build statement from pattern

---
+++ yuclid/src/statement/statement_builder.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "rules/rule_mapping.hpp"
+#include "rules/rule_schema.hpp"
+#include "statement/statement.hpp"
+
+namespace Yuclid {
+    [[nodiscard]] std::unique_ptr<Statement> build_statement_from_pattern(
+        const RulePredicatePattern &pattern,
+        const RuleMapping &mapping
+    );
+}
