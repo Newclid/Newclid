722a256bab87547078ebab25e5e56d3cef9792c5
Simeon Vutov <s.vutov@student.tudelft.nl>
2026-05-07 16:57:32 +0200

    Added one missing import

--- yuclid/src/statement/statement_builder.hpp
+++ yuclid/src/statement/statement_builder.hpp
@@ -3,6 +3,7 @@
 #include "rules/rule_mapping.hpp"
 #include "rules/rule_schema.hpp"
 #include "statement/statement.hpp"
+#include <memory>
 
 namespace Yuclid {
     [[nodiscard]] std::unique_ptr<Statement> build_statement_from_pattern(
