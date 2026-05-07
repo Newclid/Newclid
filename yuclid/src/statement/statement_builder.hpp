b159033a846a362eeb1c0df2fca30a9741e4d9e0
Simeon Vutov <s.vutov@student.tudelft.nl>
2026-05-07 19:44:42 +0200

    Just a small style fix of the includes

--- yuclid/src/statement/statement_builder.hpp
+++ yuclid/src/statement/statement_builder.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
+#include <memory>
+
 #include "rules/rule_mapping.hpp"
 #include "rules/rule_schema.hpp"
 #include "statement/statement.hpp"
-#include <memory>
 
 namespace Yuclid {
     [[nodiscard]] std::unique_ptr<Statement> build_statement_from_pattern(
