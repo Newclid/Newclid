9cb09b88d2fd9fd2628f277350c7ee0ad3be7e42
Simeon Vutov <s.vutov@student.tudelft.nl>
2026-05-07 16:58:32 +0200

    Added helper function which will search for specific variable in the mapping and return the correct point from it

--- yuclid/src/statement/statement_builder.cpp
+++ yuclid/src/statement/statement_builder.cpp
@@ -3,9 +3,9 @@
 #include "rules/rule_mapping.hpp"
 #include "rules/rule_schema.hpp"
 #include "statement/statement.hpp"
-#include <format>
 
 namespace Yuclid {
+namespace {
 
     void check_arity(const RulePredicatePattern &pattern, std::size_t expected) {
         if(pattern.args.size() != expected) {
@@ -14,11 +14,33 @@ namespace Yuclid {
                     pattern.name,
                     expected,
                     pattern.args.size()
-                )
-            );
+                    )
+                );
         }
     }
 
+    Point mapped_point(
+        const RulePredicatePattern &pattern,
+        const RuleMapping &mapping,
+        std::size_t index
+    ) {
+        const std::string &variable = pattern.args.at(index);
+
+        RuleMapping::const_iterator iterator = mapping.find(variable);
+        if(iterator == mapping.end()) {
+            std::runtime_error(
+                std::format(
+                    "Predicate {} uses variable {}, but it is missing from rule mapping.",
+                    pattern.name,
+                    variable
+                )
+            );
+        }
+
+        return iterator->second;
+    }
+
+}
     std::unique_ptr<Statement> build_statement_from_pattern(
         const RulePredicatePattern &pattern,
         const RuleMapping &mapping
