ec75d2293623dcc1b24bdaf59fcb51e2d4f7a12c
Simeon Vutov <s.vutov@student.tudelft.nl>
2026-05-08 15:52:19 +0200

    Added the definition of theorem builder

---
+++ yuclid/src/rules/theorem_builder.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "rules/rule_mapping.hpp"
+#include "rules/rule_schema.hpp"
+#include "theorem.hpp"
+
+namespace Yuclid {
+    
+    /**
+     * @brief Create a theorem from rule schema and specific mapping of points
+     */
+    [[nodiscard]] Theorem build_theorem_from_rule_schema(
+        RuleSchema &schema,
+        RuleMapping &mapping
+    );
+
+}
