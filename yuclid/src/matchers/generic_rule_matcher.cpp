f2b87390dcdd2638da7bd5c25e9120fa643fbfe7
Petar Iliev <p.g.iliev@student.tudelft.nl>
2026-05-12 16:08:38 +0200

    Implement the naive generic rule matcher.

---
+++ yuclid/src/matchers/generic_rule_matcher.cpp
@@ -0,0 +1,72 @@
+#include "matchers/generic_rule_matcher.hpp"
+
+#include "problem.hpp"
+#include "rules/rule_schema.hpp"
+#include "rules/rule_mapping.hpp"
+#include "theorem.hpp"
+#include "rules/theorem_builder.hpp"
+
+#include <boost/log/trivial.hpp>
+#include <span>
+
+using namespace std;
+
+namespace Yuclid {
+    namespace {
+        void generate_distinct_mappings(const std::vector<std::string>& rule_variables, 
+                                        size_t current_var_index, 
+                                        const std::vector<Point> &all_problem_points,
+                                        std::vector<bool> &used_points,
+                                        RuleMapping &current_mapping,
+                                        std::vector<RuleMapping> &valid_results) {
+            
+            if(current_var_index == rule_variables.size()) {
+                valid_results.push_back(current_mapping);
+                return;
+            }
+
+            const std::string& current_var = rule_variables[current_var_index];
+
+            for(size_t i = 0; i < all_problem_points.size(); ++i){
+                if(used_points[i]){
+                    continue;
+                }
+
+                used_points[i] = true;
+                current_mapping[current_var] = all_problem_points[i];
+
+                generate_distinct_mappings(rule_variables, current_var_index + 1, all_problem_points, used_points, current_mapping, valid_results);
+                
+                used_points[i] = false;
+                current_mapping.erase(current_var);
+            }
+        }
+    }
+
+    GenericRuleMatcher::GenericRuleMatcher(const Problem *prob, std::span<const RuleSchema> rules) :
+    m_problem(prob), m_rules(rules) {
+    }
+
+    std::vector<Theorem> GenericRuleMatcher::match() const{
+        std::vector<Theorem> generated_theorems;
+        for(const RuleSchema &rule_schema: m_rules){
+            std::vector<RuleMapping> &rule_mappings = find_mappings_for_rule(rule_schema);
+
+            for(const RuleMapping &mapping: rule_mappings){
+                generated_theorems.push_back(build_theorem_from_rule_schema(rule_schema, mapping));
+            }
+        }
+        return generated_theorems;
+    }
+
+
+    std::vector<RuleMapping> GenericRuleMatcher::find_mappings_for_rule(const RuleSchema *schema) const {
+        std::vector<RuleMapping> results;
+        RuleMapping current_mapping;
+        const std::vector<Point> &all_problem_points = m_problem->all_points();
+        std::vector<bool> used_points(all_problem_points.size(), false);
+    
+        generate_distinct_mappings(schema.variables, 0, all_problem_points, used_points, current_mapping, results);
+        return results;
+    }
+}
