53b21396a1b3ddbdc7bb0b8297f0711e7c2cf549
Petar Iliev <p.g.iliev@student.tudelft.nl>
2026-06-05 13:42:53 +0200

    feat: Implement is_satisfied logic for base provider.

--- yuclid/src/matchers/base_provider.cpp
+++ yuclid/src/matchers/base_provider.cpp
@@ -1,6 +1,7 @@
 #include "base_provider.hpp"
 #include "statement/statement_builder.hpp"
 #include "predicate_matching_metadata.hpp"
+#include "statement/statement.hpp"
 
 namespace Yuclid {
     std::size_t BaseProvider::estimate_extensions(
@@ -10,5 +11,27 @@ namespace Yuclid {
         [[maybe_unused]] PredicateMatchingMetadata predicate_metadata
     ) const {
         return std::numeric_limits<std::size_t>::max();
+    }
+
+    bool BaseProvider::is_satisfied(
+        const RulePredicatePattern &pattern,
+        const MappingState &mapping,
+        [[maybe_unused]] const ProblemGeometryCache &cache
+    ) const {
+        auto optional_rule_mapping = mapping.to_partial_rule_mapping();
+
+        if (!optional_rule_mapping.has_value()) {
+            return false;
+        }
+
+        auto statements = build_statements_from_pattern(pattern, optional_rule_mapping.value());
+        
+        for(const auto &statement : statements) {
+            if(!statement->check_numerically()){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
