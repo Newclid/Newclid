4518ed565d305aee588345fcdf82246084dc9f5f
Petar Iliev <p.g.iliev@student.tudelft.nl>
2026-06-05 16:09:12 +0200

    feat: Change estimate_extensions logic to actually calculate number of naive extensions.

--- yuclid/src/matchers/base_provider.cpp
+++ yuclid/src/matchers/base_provider.cpp
@@ -22,6 +22,25 @@ namespace Yuclid {
             }
             return extension;
         }
+
+        std::size_t variation_with_cap(std::size_t size_of_set, std::size_t num_total_elements) {
+            if (size_of_set > num_total_elements) return 0;
+
+            constexpr std::size_t MAX_ESTIMATE_CAP = 100'000'000;
+            std::size_t result = 1;
+            std::size_t next_element;
+
+            for (std::size_t i = 0; i < size_of_set; ++i) {
+                next_element = num_total_elements - i;
+                
+                if (result > MAX_ESTIMATE_CAP / next_element) {
+                    return MAX_ESTIMATE_CAP;
+                }
+
+                result *= next_element;
+            }
+            return result;
+        }
     }
 
 
@@ -29,20 +48,24 @@ namespace Yuclid {
         [[maybe_unused]] const RulePredicatePattern &pattern,
         [[maybe_unused]] const MappingState &mapping,
         [[maybe_unused]] const ProblemGeometryCache &cache,
-        [[maybe_unused]] const PredicateMatchingMetadata predicate_metadata
+        [[maybe_unused]] const PredicateMatchingMetadata &predicate_metadata
     ) const {
-        return std::numeric_limits<std::size_t>::max();
+        // TODO get the total number of points, once the cache is actually implemented
+        std::size_t num_unassigned_vars = mapping.unassigned_variables().size();
+        std::size_t num_free_points; // = cache.get_problem()->num_points() - unassigned_vars;
+        std::size_t estimate = predicate_metadata.base_cost + variation_with_cap(num_unassigned_vars, num_free_points);
+
+        return estimate;
     }
 
     std::generator<MappingExtension> BaseProvider::generate_extensions(
-        const RulePredicatePattern &pattern,
+        [[maybe_unused]] const RulePredicatePattern &pattern,
         const MappingState &mapping,
-        [[maybe_unused]] const ProblemGeometryCache &cache,
+        [[maybe_unused]] const ProblemGeometryCache &cache
     ) const {
         // TODO get the total number of points, once the cache is actually implemented
         std::size_t num_points; // = cache.get_problem()->num_points();
         std::vector<RuleVariableIndex> unassigned_vars = mapping.unassigned_variables();
-
         std::vector<std::size_t> free_points;
 
         for(std::size_t i = 0; i < num_points; ++i){
@@ -58,7 +81,7 @@ namespace Yuclid {
         std::vector<std::size_t>::iterator nth_position = free_points.begin() + unassigned_vars.size();
 
 
-        while(std::next_partial_permutation(free_points.begin(), nth_position, free_points.end())){
+        while(next_partial_permutation(free_points.begin(), nth_position, free_points.end())){
             MappingExtension next_extension = get_extension_from_permutation(unassigned_vars, free_points);
             co_yield next_extension;
         }
