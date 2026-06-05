06b9cfdf7f42809a727518e5a055d61ac239bb62
Petar Iliev <p.g.iliev@student.tudelft.nl>
2026-06-05 13:05:44 +0200

    feat: Add base provider header and add nodiscard attribute to generator.

---
+++ yuclid/src/matchers/base_provider.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "predicate_provider.hpp"
+#include "predicate_matching_metadata.hpp"
+#include "mapping_state.hpp"
+
+namespace Yuclid {
+
+    class BaseProvider : public PredicateProvider {
+    public:
+        [[nodiscard]] std::size_t estimate_extensions(
+            const RulePredicatePattern &pattern,
+            const MappingState &mapping,
+            const ProblemGeometryCache &cache,
+            PredicateMatchingMetadata predicate
+        ) const override;
+
+        [[nodiscard]] std::generator<MappingExtension> generate_extensions(
+            const RulePredicatePattern &pattern,
+            const MappingState &mapping,
+            const ProblemGeometryCache &cache
+        ) const override;
+
+        [[nodiscard]] bool is_satisfied(
+            const RulePredicatePattern &pattern,
+            const MappingState &mapping,
+            const ProblemGeometryCache &cache
+        ) const override;
+    };
+
+}
