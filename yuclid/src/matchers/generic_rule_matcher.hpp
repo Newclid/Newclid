ac6d6a7518c98f09c9fe090e92e8e0ee3422d90b
Petar Iliev <p.g.iliev@student.tudelft.nl>
2026-06-11 01:26:42 +0200

    feat: Declare necessary methods.

--- yuclid/src/matchers/generic_rule_matcher.hpp
+++ yuclid/src/matchers/generic_rule_matcher.hpp
@@ -4,6 +4,8 @@
 #include "rules/rule_schema.hpp"
 #include "rules/rule_mapping.hpp"
 #include "theorem.hpp"
+#include "predicate_provider.hpp"
+#include "filter_state.hpp"
 
 #include <span>
 
@@ -13,13 +15,25 @@ namespace Yuclid {
             GenericRuleMatcher(const Problem *prob, std::span<const RuleSchema> rules);
 
             [[nodiscard]] std::vector<Theorem> match() const;
+            [[nodiscard]] std::vector<Theorem> optimized_match() const;
 
         private:
             const Problem *m_problem;
             std::span<const RuleSchema> m_rules;
+            PredicateProviderRegistry m_provider_registry;
 
-            [[nodiscard]] std::vector<RuleMapping> find_mappings_for_rule(const RuleSchema &schema) const;
             //Safety limit for naive matching
             static constexpr size_t MAX_VARIABLES_FOR_NAIVE_MATCHING = 6;
+            [[nodiscard]] std::vector<RuleMapping> find_mappings_for_rule(const RuleSchema &schema) const;
+            [[nodiscard]] std::vector<Theorem> build_valid_theorems_from_mappings(const RuleSchema &schema, const std::vector<RuleMapping> &mappings) const;
+            void add_providers_to_registry();
+            void search(
+                const RulePlan &plan,
+                MappingState &current_state,
+                FilterState &filter_state,
+                LazyGeometryCache &geometry_cache,
+                std::vector<RuleMapping> &results
+            ) const;
+            const PlannedPredicate* get_cheapest_predicate(const std::vector<PlannedPredicate> &predicates, const MappingState &state, LazyGeometryCache &cache) const;
     };
 }
