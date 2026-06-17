33f8c01290821569f4744bb4c19dc91e2df08010
Petar Iliev <p.g.iliev@student.tudelft.nl>
2026-06-17 04:07:08 +0200

    feat: Add cong_provider class, implement estimate_extensions and helpers.

--- yuclid/src/matchers/cong_provider.hpp
+++ yuclid/src/matchers/cong_provider.hpp
@@ -2,14 +2,14 @@
 
 #include "predicate_provider.hpp"
 #include "mapping_state.hpp"
-#include "lazy_geometry_cache.hpp"
+#include "lazy_geometry_cache.hpp"         
 
 namespace Yuclid {
 
-    /** @brief This class represents the default Provider.
-     * It will be used for predicates that dont have a specific one implemented.
+    /** @brief This class implements the congruence provider.
+     * It will be used for the atomic 'cong' predicate (with only 3 variables cong A B C). 
     */
-    class BaseProvider : public PredicateProvider {
+    class CongProvider : public PredicateProvider {
     public:
         [[nodiscard]] std::size_t estimate_extensions(
             const PlannedPredicate &predicate,
